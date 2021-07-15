8d490a3ee97ab069464fb9e68845290a2d2ddc5b
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-07-15 18:27:27 -0400

    Update ModemStart.lua

--- ModemStart.lua
+++ ModemStart.lua
@@ -1,11 +1,12 @@
-term.clear()
-print("What channel do you want to recieve?")
-port = tonumber(io.read())
-modem = peripheral.wrap("left")
-modem.open(port)
+term.clear();
+print("Channel to recieve:");
+port = tonumber(io.read());
+modem = peripheral.wrap("left");
+modem.open(port);
 
-while true do
-  event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-  print("Message recieved on channel "..senderChannel)
-  print(message)
+while (true) do
+    event,modemSide,senderChannel,replyChannel,message,senderDistance = os.pullEvent("modem_message");
+
+    print("Message from channel "..senderChannel)
+    print("message");
 end
