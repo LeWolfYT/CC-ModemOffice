5f7cf0d0787d76a484438a0c69072d30e3eda2ca
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-07-15 18:35:29 -0400

    make pocket computers work

--- ModemSend.lua
+++ ModemSend.lua
@@ -2,7 +2,11 @@ print("Channel:");
 port = tonumber(io.read());
 print("message:");
 message = io.read();
-modem = peripheral.wrap("left");
+if (peripheral.wrap("left") == nil)
+  modem = back;
+else
+  modem = modem = peripheral.wrap("left");
+end
 modem.open(port);
 model.closeAll();
 modem.transmit(port, 1, message);
