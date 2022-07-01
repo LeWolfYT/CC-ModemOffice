ccbed5842a77ff03c13340121a6b54ea25e2adc3
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-07-01 09:49:45 -0400

    Update ModemSend.lua

--- ModemSend.lua
+++ ModemSend.lua
@@ -2,11 +2,7 @@ print("What channel will this message go to?");
 port = tonumber(io.read());
 print("Please type a message");
 message = io.read();
-if peripheral.wrap("left")~=nil then
-  modem = peripheral.wrap("left")
-else
-  modem = peripheral.wrap("back")
-end
+modem = peripheral.find("modem")
 modem.open(port);
 modem.closeAll();
 modem.transmit(port, 1, message);
