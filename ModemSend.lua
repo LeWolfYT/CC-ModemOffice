80d1fd8111cf980414681dea743b935b9e92ab71
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-08-30 07:03:40 -0400

    Update ModemSend.lua

--- ModemSend.lua
+++ ModemSend.lua
@@ -1,10 +1,10 @@
-print("ModemOffice v1.1")
-print("What channel will this message go to?");
-port = tonumber(read());
-print("Please type a message");
-message = read();
+print("ModemOffice v1.2")
+print("What channel will this message go to?")
+port = tonumber(read())
+print("Please type a message")
+message = read()
 modem = peripheral.find("modem")
-modem.open(port);
-modem.closeAll();
-modem.transmit(port, 1, "<" .. os.getComputerLabel() .. ">" .. message);
-term.clear();
+modem.open(port)
+modem.closeAll()
+modem.transmit(port, 1, "<" .. os.getComputerLabel() .. ">" .. message)
+term.clear()
