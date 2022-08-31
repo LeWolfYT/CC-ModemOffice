297446e7b4dcc74bde9127dcfc898c5afacd5d05
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-08-31 07:20:10 -0400

    Update ModemOffice.lua

--- ModemOffice.lua
+++ ModemOffice.lua
@@ -1,7 +1,10 @@
 local w,h = term.getSize()
-
-local winone = window.create(term.current(),1,1,w,math.floor(h/2))
-local wintwo = window.create(term.current(),1,math.ceil(h/2),w,math.ceil(h/2))
+paintutils.drawLine(1,1,w,1,colors.cyan)
+term.setCursorPos(1,1)
+local toptext = "ModemOffice v1.2"
+term.blit(toptext,(string.rep("f",#toptext)),(string.rep("9",#toptext)))
+local winone = window.create(term.current(),1,2,w,(math.floor(h/2) - 1))
+local wintwo = window.create(term.current(),1,(math.ceil(h/2) + 1),w,(math.ceil(h/2) - 1))
 
 winone.setBackgroundColor(colors.gray)
 winone.setTextColor(colors.white)
@@ -11,17 +14,26 @@ wintwo.setBackgroundColor(colors.lightGray)
 local function get()
   term.redirect(winone)
   print("What channel will this message go to?")
-  port = tonumber(read())
+  local port = tonumber(read())
   print("Please type a message")
-  message = read()
+  local message = read()
   modem = peripheral.find("modem")
   modem.open(port)
   modem.closeAll()
   modem.transmit(port, 1, "<" .. os.getComputerLabel() .. ">" .. message)
 end
 local function send()
-  while true do
     term.redirect(wintwo)
+    term.clear()
+    term.setCursorPos(1,1)
+    print("What channel do you want to recieve?")
+    local port = tonumber(io.read())
+    modem = peripheral.find("modem")
+    modem.open(port)
+  term.clear()
+  while true do
+    local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    print(message)
   end
 end
 parallel.waitForAll(get,send)
