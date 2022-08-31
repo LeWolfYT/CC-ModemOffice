80683863725592c98e4d2787011f778909ac4f8d
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2022-08-31 07:08:03 -0400

    Update ModemOffice.lua

--- ModemOffice.lua
+++ ModemOffice.lua
@@ -10,7 +10,14 @@ wintwo.setBackgroundColor(colors.lightGray)
 
 local function get()
   term.redirect(winone)
-  shell.run("./ModemStart.lua")
+  print("What channel will this message go to?")
+  port = tonumber(read())
+  print("Please type a message")
+  message = read()
+  modem = peripheral.find("modem")
+  modem.open(port)
+  modem.closeAll()
+  modem.transmit(port, 1, "<" .. os.getComputerLabel() .. ">" .. message)
 end
 local function send()
   while true do
